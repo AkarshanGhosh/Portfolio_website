b1211c5fd16a5ba0b7af31e1fb53a225094dc8a7
AkarshanGhosh <ronighosh494@gmail.com>
2024-08-02 23:39:01 +0530

    changing in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,19 @@ tailwind.config = {
         extend: {
             gridTemplateColumns: {
                 'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+            },
+            animation:{
+                spin_slow: 'spin 6s linear infinite'
+            },
+            colors:{
+                lightHover: '#fcf4ff',
+                darkHover: '#2a004a',
+                darkTheme: '#1101F'
+            },
+            boxShadow:{
+                'black': '4px 4px 0 #000',
+                'white': '4px 4px 0 #fff',
             }
-        },
-        animation:{
-            spin_slow: 'spin 6s linear infinite'
         }
     }
 }
