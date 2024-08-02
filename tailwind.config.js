136d07cb33f3bb6ef12b328074c3f97307090952
AkarshanGhosh <ronighosh494@gmail.com>
2024-08-02 23:26:27 +0530

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ tailwind.config = {
             gridTemplateColumns: {
                 'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
             }
+        },
+        animation:{
+            spin_slow: 'spin 6s linear infinite'
         }
     }
 }
